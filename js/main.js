944339cbd99e828e10e188a8ea2963699b87d788
strambofort <strambofort@gmail.com>
2021-03-09 23:44:22 +0300

    create: yandex api maps

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
-const swiper = new Swiper('.swiper-container', {
-  // Optional parameters  
+const swiper = new Swiper(".swiper-container", {
+  // Optional parameters
   loop: true,
 
   // Navigation arrows
   navigation: {
-    nextEl: '.slider-button--next',
-    prevEl: '.slider-button--prev',
+    nextEl: ".slider-button--next",
+    prevEl: ".slider-button--prev",
   },
-    keyboard: {
+  keyboard: {
     enabled: true,
     onlyInViewport: false,
   },
-
+});
+ymaps.ready(function () {
+  // Указывается идентификатор HTML-элемента.
+  var hotel_map = new ymaps.Map("map", {
+    center: [6.93, 79.84],
+    zoom: 10,
+  });
 });
