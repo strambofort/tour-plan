2d7752714ea2e06a468b27d4d4d591b80808d680
strambofort <strambofort@gmail.com>
2021-03-17 17:53:14 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -26,10 +26,16 @@ const reviewsSlider = new Swiper(".reviews-slider", {
     onlyInViewport: false,
   },
 });
-ymaps.ready(function () {
-  // Указывается идентификатор HTML-элемента.
-  var hotel_map = new ymaps.Map("map", {
-    center: [6.93, 79.84],
-    zoom: 10,
-  });
+// ymaps.ready(function () {
+//   // Указывается идентификатор HTML-элемента.
+//   var hotel_map = new ymaps.Map("map", {
+//     center: [6.93, 79.84],
+//     zoom: 10,
+//   });
+// });
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", function () {
+  console.log("Клик по кнопле меню");
+  document.querySelector('.navbar-botton').classList.toggle('navbar-botton--visible');
 });
